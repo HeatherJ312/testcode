4460c9797972d2d80d9f53b000a4f7aaa0b28d3d
Heather Johnson <heatherjohnson312@gmail.com>
2025-01-17 11:20:33 +0000

    Added Sam and updates ages

--- TrelloTest.py
+++ TrelloTest.py
@@ -1,7 +1,8 @@
 list=[
-    {'name': 'Harry', 'id': 2, 'date': 'April'}, 
-    {'name': 'Chris', 'id': 34, 'date': 'May'},
-    {'name': 'Heather', 'id':33, 'date': 'September'}
+    {'name': 'Harry', 'id': 4, 'date': 'April'}, 
+    {'name': 'Chris', 'id': 36, 'date': 'May'},
+    {'name': 'Heather', 'id':36, 'date': 'September'}
+    {'name': 'Sam', 'id':1, 'date': 'December'}
 ]
 
 
